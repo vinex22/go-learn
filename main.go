f35d3613c9e06db9d9170aa6adb634c5d8e64c08
vinex22 <35246234+vinex22@users.noreply.github.com>
2023-04-16 23:56:04 +0800

    v

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ func main() {
 	var b float64 = 1.23
 	var c bool = true
 	var d string = "vin"
-	const e = "msn"
-
-	fmt.Printf("Variable %v is type of %T\n", a, a)
-	fmt.Printf("Variable %v is type of %T\n", b, b)
-	fmt.Printf("Variable %t is type of %T\n", c, c)
-	fmt.Printf("Variable %v is type of %T\n", d, d)
-	fmt.Printf("Variable %v is type of %T\n", e, e)
-	fmt.Printf("Variable %f is type of %T\n", f, f)
+	const e = "vin"
+	g := (d == e)
+	fmt.Printf("Variable a = %v is type of %T\n", a, a)
+	fmt.Printf("Variable b = %v is type of %T\n", b, b)
+	fmt.Printf("Variable c = %t is type of %T\n", c, c)
+	fmt.Printf("Variable d = %v is type of %T\n", d, d)
+	fmt.Printf("Variable e = %v is type of %T\n", e, e)
+	fmt.Printf("Variable f = %f is type of %T\n", f, f)
+	fmt.Printf("Variable g  = %v is type of %T\n", g, g)
 
 }
