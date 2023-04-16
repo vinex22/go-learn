dbe1a6a7fd7e576fd1b56538c4aafbeb8f3fa851
vinex22 <35246234+vinex22@users.noreply.github.com>
2023-04-16 23:39:54 +0800

    v

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi Vin")
+	var a int = 1
+	f := float64(a)
+	var b float64 = 1.23
+	var c bool = true
+	var d string = "vin"
+	const e = "msn"
+
+	fmt.Printf("Variable %v is type of %T\n", a, a)
+	fmt.Printf("Variable %v is type of %T\n", b, b)
+	fmt.Printf("Variable %t is type of %T\n", c, c)
+	fmt.Printf("Variable %v is type of %T\n", d, d)
+	fmt.Printf("Variable %v is type of %T\n", e, e)
+	fmt.Printf("Variable %f is type of %T\n", f, f)
+
 }
