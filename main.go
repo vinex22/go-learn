ed65a524b315ef5907a7f83a7e7e9f93d7af77b5
vinex22 <35246234+vinex22@users.noreply.github.com>
2023-04-17 10:16:47 +0800

    f

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ func main() {
 	f := float64(a)
 	var b float64 = 1.23344
 	var c bool = true
-	var d string = "vin"
-	const e = "vin"
+	var d string = "a"
+	const e = "b"
 	g := (d == e)
+	h := d + e
 	fmt.Printf("Variable a = %v is type of %T\n", a, a)
 	fmt.Printf("Variable b = %v is type of %T\n", b, b)
 	fmt.Printf("Variable c = %t is type of %T\n", c, c)
@@ -17,5 +18,5 @@ func main() {
 	fmt.Printf("Variable e = %v is type of %T\n", e, e)
 	fmt.Printf("Variable f = %f is type of %T\n", f, f)
 	fmt.Printf("Variable g  = %v is type of %T\n", g, g)
-
+	fmt.Printf("Concatination of d and e is %X\n ", h)
 }
